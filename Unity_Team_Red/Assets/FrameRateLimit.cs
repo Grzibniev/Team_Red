7ac7df3c683d32883ef911960f1f3dff4610dd4a
Grzib <Wiertelarz21@wp.pl>
2023-03-22 18:42:02 +0100

    Unity

---
+++ Unity_Team_Red/Assets/FrameRateLimit.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameRateLimit : MonoBehaviour
+{
+   public enum limits
+   {
+
+        nolimit = 0,
+        limit30 = 30,
+        limit60 = 60,
+
+   }
+
+    public limits limit;
+
+    void Awake()
+    {
+        Application.targetFrameRate = (int)limit;
+    }
+}
