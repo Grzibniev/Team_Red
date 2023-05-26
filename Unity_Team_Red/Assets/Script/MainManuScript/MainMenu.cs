4cbe093db675db897b7cf2db78b6099182b6154a
Grzib <Wiertelarz21@wp.pl>
2023-05-26 02:49:31 +0200

    Unity(naprawiony mainmenu i zrobione opcje))

--- Unity_Team_Red/Assets/Script/MainManuScript/MainMenu.cs
+++ Unity_Team_Red/Assets/Script/MainManuScript/MainMenu.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MainMenu : MonoBehaviour
 {
-   public void PlayGame()
+    public void PlayGame ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
     {
+        Debug.Log("Quit");
         Application.Quit();
     }
-
 }
