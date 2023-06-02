996667d537612c79626694acc46bcca996db973c
Grzib <Wiertelarz21@wp.pl>
2023-06-02 14:17:47 +0200

    Unity(Dużo nowych pułapek)

---
+++ Unity_Team_Red/Assets/Script/Enemy/FireTrap.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using System.Collections;
+
+public class FireTrap : MonoBehaviour
+{
+    [SerializeField] private float damage;
+
+    [Header("FireTrap Timers")]
+    [SerializeField] private float activationDelay;
+    [SerializeField] private float activeTime;
+    private Animator anim;
+    private SpriteRenderer spriteRend;
+
+    private bool triggered;
+    private bool active;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (!triggered)
+                StartCoroutine(ActivateFiretrap());
+
+            if (active)
+                collision.GetComponent<Health>().TakeDamage(damage);
+        }
+    }
+        private  IEnumerator ActivateFiretrap()
+    {
+            //zmienia kolor pu³apki na czerwony 
+            triggered = true;
+            spriteRend.color = Color.red;
+
+            //czeka na opóznienie, aktywuje pu³apke,aktywuje animacje,zmienia kolor na podstawowy 
+            yield return new WaitForSeconds(activationDelay);
+            spriteRend.color = Color.white;
+            active = true;
+            anim.SetBool("activated", true);
+
+            //czeka x sekund, wy³¹cza pu³ake i resetuje wartosci
+            yield return new WaitForSeconds(activeTime);
+            active = false;
+            triggered = false;
+        anim.SetBool("activated", false);
+    }
+    
+
+    
+}
---
+++ Unity_Team_Red/Assets/Script/Enemy/TurretTrap.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class TurretTrap : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] bullets;
+    private float cooldownTimer;
+
+    private void Attack()
+    {
+        cooldownTimer = 0;
+
+        bullets[FindBullets()].transform.position = firePoint.position;
+        bullets[FindBullets()].GetComponent<EnemyProjectile>().ActivateProjectile();
+     }
+
+    private int FindBullets()
+    {
+        for (int i = 0; i < bullets.Length; i++)
+        {
+            if (!bullets[i].activeInHierarchy)
+                return i;
+        }
+        return 0;
+    }
+
+    private void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+        if (cooldownTimer >= attackCooldown)
+            Attack();
+    }
+
+}
